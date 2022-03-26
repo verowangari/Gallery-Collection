ad3916970e7f9dc88f45f87b02db99992a5af9e3
verowangari <verowangari34@gmail.com>
2022-03-26 13:31:15 +0300

    create templates

--- photos/views.py
+++ photos/views.py
@@ -4,7 +4,7 @@ import datetime as dt
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to the GalleryShare')
+    return render(request, 'welcome.html')
 
 def photo_of_day(request):
     date = dt.date.today()
