d6acacc103d72f62de383221c8f1af7a489b72e2
verowangari <verowangari34@gmail.com>
2022-03-26 13:24:49 +0300

    add app congiguration to the project configuration

--- photos/urls.py
+++ photos/urls.py
@@ -3,5 +3,7 @@ from django.urls import re_path
 from . import views
 urlpatterns=[
     re_path('^$',views.welcome,name = 'welcome'),
-    re_path('^today/$',views.photo_of_day,name='newsToday')
+    re_path('^today/$',views.photo_of_day,name='newsToday'),
+    re_path(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_photos,name = 'pastPhotos')
+    
 ]
--- photos/views.py
+++ photos/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http  import HttpResponse
+from django.http  import HttpResponse,Http404
 import datetime as dt
 
 # Create your views here.
@@ -8,10 +8,44 @@ def welcome(request):
 
 def photo_of_day(request):
     date = dt.date.today()
+
+    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
+    day = convert_dates(date)
     html = f'''
         <html>
             <body>
-                <h1> {date.day}-{date.month}-{date.year}</h1>
+                <h1>Photos for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
+
+def convert_dates(dates):
+
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
+
+def past_days_photos(request,past_date):
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+    
+    
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>Photos for {day} {date.day}-{date.month}-{date.year}</h1>
             </body>
         </html>
             '''
