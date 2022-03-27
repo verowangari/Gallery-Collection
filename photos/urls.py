362c868c63d4cb430cf1c5eaaeea692f7b37e0ea
verowangari <verowangari34@gmail.com>
2022-03-27 10:35:26 +0300

    add css file for gallery html

--- photos/urls.py
+++ photos/urls.py
@@ -2,7 +2,7 @@ import re
 from django.urls import re_path
 from . import views
 urlpatterns=[
-    re_path('^$',views.welcome,name = 'welcome'),
+    # re_path('^$',views.welcome,name = 'welcome'),
     re_path('^today/$',views.photos_of_day,name='newsToday'),
     re_path(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_photos,name = 'pastPhotos'),
     re_path('',views.gallery,name='gallery'),
