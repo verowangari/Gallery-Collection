b206bab0418680ad9785d48703723ecf9e2dd784
verowangari <verowangari34@gmail.com>
2022-03-27 17:28:31 +0300

    configure image urls

--- gallery/settings.py
+++ gallery/settings.py
@@ -122,11 +122,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+STATICFILES_DIRS = [
+    # os.path.join(BASE_DIR, 'static'),
+    BASE_DIR/'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL='/images/'
+MEDIA_ROOT =BASE_DIR/'static/images'
+STATIC_ROOT =BASE_DIR / 'staticfiles'
