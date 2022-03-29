302b85725b266306fccfe5b2eceb8f12445e20bf
verowangari <verowangari34@gmail.com>
2022-03-29 13:41:44 +0300

    deploy4

--- gallery/settings.py
+++ gallery/settings.py
@@ -27,13 +27,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ur1$s3&1c_&s3hz_-jfhhp&4lvidth4nn##qzf2mf@9aq@6*s+'
 
+
+# configurations for deployment
+MODE=config("MODE", default="dev")
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = ['*']
 
+ALLOWED_HOSTS = '*'
 
 # Application definition
 
@@ -81,6 +85,29 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'gallery.wsgi.application'
+
+# development
+if config('MODE')=="dev":
+   DATABASES = {
+       'default': {
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': config('DB_NAME'),
+           'USER': config('DB_USER'),
+           'PASSWORD': config('DB_PASSWORD'),
+           'HOST': config('DB_HOST'),
+           'PORT': '5432',
+       }
+       
+   }
+# production
+else:
+   DATABASES = {
+       'default': dj_database_url.config(
+           default=config('DATABASE_URL')
+       )
+   }
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Database
@@ -130,7 +157,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+# deploy root
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
+
+# path from deployment
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
 # Working path in development
 # STATICFILES_DIRS = [
 #     # os.path.join(BASE_DIR, 'static'),
@@ -139,11 +177,6 @@ STATIC_URL = '/static/'
 # ]
 
 
-# path from deployment
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -173,8 +206,7 @@ django_heroku.settings(locals())
 # working root for development
 # STATIC_ROOT =BASE_DIR / 'staticfiles'
 
-# deploy root
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 
 # adding config
@@ -185,30 +217,4 @@ cloudinary.config(
 )
 
 
-# configurations for deployment
-MODE=config("MODE", default="dev")
-SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
-# development
-if config('MODE')=="dev":
-   DATABASES = {
-       'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'gallery',
-        'USER':'moringa',
-        'PASSWORD':'Access',
-    }
-       
-   }
-# production
-else:
-   DATABASES = {
-       'default': dj_database_url.config(
-           default=config('DATABASE_URL')
-       )
-   }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
